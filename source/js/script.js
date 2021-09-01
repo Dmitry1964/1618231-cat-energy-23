d5fae7ac93dd98bdd447c01bef9e5cfe62943c90
Dmitry1964 <nosdm64@gmail.com>
2021-09-01 21:20:40 +0300

    Добавляет декоративные элементы главной страницы

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+const headerButton = document.querySelector(".page-header__button");
+const pageNavigation = document.querySelector(".page-header__navigation");
+
+pageNavigation.classList.remove("page-header__navigation--nojs");
+
+headerButton.addEventListener("click", function () {
+  if (headerButton.classList.contains("page-header__button--open")) {
+    headerButton.classList.remove("page-header__button--open");
+    headerButton.classList.add("page-header__button--close");
+    pageNavigation.classList.remove("page-header__navigation--closed");
+
+  } else {
+    headerButton.classList.add("page-header__button--open");
+    headerButton.classList.remove("page-header__button--close");
+    pageNavigation.classList.add("page-header__navigation--closed");
+  }
+})
+
+console.log(pageNavigation);
