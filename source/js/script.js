523ec6f1d4e9622c6044969dcf1b1bd2052d2d23
Dmitry1964 <nosdm64@gmail.com>
2021-09-16 05:18:26 +0300

    Исправлены базовые критерии

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ const headerButton = document.querySelector(".page-header__button");
 const pageNavigation = document.querySelector(".page-header__navigation");
 
 pageNavigation.classList.remove("page-header__navigation--nojs");
+headerButton.classList.remove("page-header__button--nojs");
 
 headerButton.addEventListener("click", function () {
   if (headerButton.classList.contains("page-header__button--open")) {
